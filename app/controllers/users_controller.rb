0ab06a3b9fc55b26026763ebf4b00e071b4c5416
tranvietsi <tran.viet.si@sun-asterisk.com>
2020-09-29 19:37:16 +0700

    Chapter 10: Editing, showing and deleting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  before_action :load_user, only: %i(show)
+  before_action :logged_in_user, except: %i(new create)
+  before_action :load_user, only: %i(show edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: %i(destroy)
 
   def show; end
 
@@ -18,6 +21,30 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = t "views.edit.success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "views.index.delete_success"
+    else
+      flash[:error] = t "views.index.delete_fail"
+    end
+    redirect_to users_url
+  end
+
   private
 
   def user_params
@@ -31,5 +58,25 @@ class UsersController < ApplicationController
 
     flash[:danger] = t "views.show.not_found"
     redirect_to root_path
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    redirect_to(root_url) unless @user == current_user
+  end
+
+  # Before filters
+  # Confirms a logged-in user.
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "models.users.please_login"
+    redirect_to login_url
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,28 +1,13 @@
 require_relative 'boot'
-
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Test1
+module SampleApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    # Config i18n
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-
-    # Cấu hình những ngôn ngữ mà chúng ta cần hiển thị
-    config.i18n.available_locales = [:en, :vi]
-
-    # Cài đặt ngôn ngữ mặc định khi chạy ứng dụng
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
     config.i18n.default_locale = :vi
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.i18n.available_locales = [:en, :vi]
   end
 end
