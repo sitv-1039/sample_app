4250b4d742d57e17554942b19ef5f810bd8017a7
tranvietsi <tran.viet.si@sun-asterisk.com>
2020-09-29 19:37:16 +0700

    Chapter 9: Advance login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       log_in user
+      # remember user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       redirect_to user_path(id: user.id)
     else
       flash.now[:danger] = t "views.login.email_pass_invalid"
@@ -13,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     flash[:success] = t "views.logout.success"
     redirect_to root_path
   end
